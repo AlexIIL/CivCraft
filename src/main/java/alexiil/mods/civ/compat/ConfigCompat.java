243b574d43209b5f64219bbca7f01e547b030ddd
AlexIIL <alexj9@me.com>
2015-02-22 21:43:45 +0000

    Changed the config compat to reflect the changes made to the tech, and properly add techs now.

--- src/main/java/alexiil/mods/civ/compat/ConfigCompat.java
+++ src/main/java/alexiil/mods/civ/compat/ConfigCompat.java
@@ -1,11 +1,9 @@
 package alexiil.mods.civ.compat;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagInt;
 import net.minecraft.nbt.NBTTagList;
-import net.minecraft.nbt.NBTTagString;
+import alexiil.mods.civ.Lib;
 import alexiil.mods.civ.tech.TechTree;
 import alexiil.mods.civ.tech.TechTree.Tech;
 import alexiil.mods.civ.tech.TechTreeEvent.AddTechs;
@@ -15,30 +13,33 @@ public class ConfigCompat extends ModCompat {
     @Override public void addTechs(AddTechs t) {}
     
     @Override public void addUnlockables(AddUnlockables t) {
+        TechTree tree = t.tree;
         NBTTagCompound n = t.treeNBTCompound;
         NBTTagCompound techs = n.getCompoundTag("techs");
         
         for (Object key : techs.getKeySet()) {
             NBTTagCompound tech = techs.getCompoundTag((String) key);
-            NBTTagList parents = tech.getTagList("parents", new NBTTagString().getId());
-            List<Tech> newParents = new ArrayList<Tech>();
-            for (int i = 0; i < parents.tagCount(); i++) {
-                NBTTagString ns = (NBTTagString) parents.get(i);
-                String s = ns.getString();
-                Tech t0 = t.tree.getTech(s);
-                if (t0 != null)
-                    newParents.add(t0);
-                else
-                    TechTree.log.warn("The tech \"" + s + "\" was null! Was that a mistype in the config (under \"" + key + "\")");
+            // Almost directly copied from TechTree.Tech.<init>
+            Tech techAdded = tree.addTech((String) key);
+            NBTTagList nbtparents = tech.getTagList("parents", Lib.NBT.STRING);
+            Tech[] parents = new Tech[nbtparents.tagCount()];
+            for (int idx = 0; idx < nbtparents.tagCount(); idx++) {
+                String parentName = nbtparents.getStringTagAt(idx);
+                Tech parent = tree.getTech(parentName);
+                techAdded.addRequirement(parent);
             }
-            Tech t1 = t.tree.getTech((String) key);
-            if (t1 != null)
-                t1.setRequirements(newParents.toArray(new Tech[newParents.size()]));
-            else
-                TechTree.log.warn("The tech \"" + key + "\" was null! Was that a mistype in the config?");
+            NBTTagList sciencePacks = tech.getTagList("sciencePacks", Lib.NBT.INTEGER);
+            int[] required = new int[sciencePacks.tagCount()];
+            for (int idx = 0; idx < sciencePacks.tagCount(); idx++) {
+                required[idx] = ((NBTTagInt) sciencePacks.get(idx)).getInt();
+            }
+            techAdded.setSciencePacksNeeded(required);
+            if (tech.getBoolean("leaf"))
+                techAdded.setLeafTech();
         }
         
         NBTTagCompound unlock = n.getCompoundTag("unlockables");
+        // TODO: loading unlockable's
     }
     
     @Override public String getModID() {
