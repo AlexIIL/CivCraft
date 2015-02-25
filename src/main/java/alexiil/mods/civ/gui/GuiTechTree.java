83a8cced6e97bef8322acda4408dd8bf2fd33c35
AlexIIL <alexj9@me.com>
2015-02-25 14:29:37 +0000

    First attempt at making GuiTechTree better

--- src/main/java/alexiil/mods/civ/gui/GuiTechTree.java
+++ src/main/java/alexiil/mods/civ/gui/GuiTechTree.java
@@ -281,93 +281,123 @@ public class GuiTechTree extends GuiScreen {
             else
                 nextList = null;
             List<Tech> currentList = techList.get(idx);
-            
-            // Basically, add a relationship between each tech and each other tech in this list.
-            Map<Tech, Map<Tech, Integer>> relationship = new HashMap<Tech, Map<Tech, Integer>>();
-            for (int j = 0; j < currentList.size(); j++) {
-                Tech t0 = currentList.get(j);
-                Map<Tech, Integer> mp = new HashMap<Tech, Integer>();
-                relationship.put(t0, mp);
-                for (int k = 0; k < currentList.size(); k++) {
-                    Tech t1 = currentList.get(k);
-                    if (t1 == t0)
-                        continue;
-                    int shared = 0;
-                    
-                    if (nextList != null) {
-                        List<Tech> t1Children = Arrays.asList(t1.getChildTechs());
-                        for (int l = 0; l < nextList.size(); l++) {
-                            Tech t = nextList.get(l);
-                            for (Tech t00 : t0.getChildTechs())
-                                if (t1Children.contains(t00))
-                                    shared++;
-                        }
-                    }
-                    
-                    List<Tech> t1Parents = Arrays.asList(t1.getParentTechs());
-                    for (int l = 0; l < lastList.size(); l++) {
-                        Tech t = lastList.get(l);
-                        for (Tech t00 : t0.getParentTechs())
-                            if (t1Parents.contains(t00))
-                                shared++;
-                    }
-                    
-                    mp.put(t1, shared);
-                }
+            // Sort the techs according to where its parents are
+            Map<Double, List<Tech>> newBests = new HashMap<Double, List<Tech>>();
+            for (Tech t : currentList) {
+                double bestPos = 0;
+                for (Tech p : t.getParentTechs())
+                    bestPos += lastList.indexOf(p);
+                bestPos /= (double) t.getParentTechs().length;
+                List<Tech> techs = newBests.containsKey(bestPos) ? newBests.get(bestPos) : new ArrayList<Tech>();
+                techs.add(t);
+                newBests.put(bestPos, techs);
             }
             
-            for (int j = 0; j < currentList.size(); j++) {
-                List<Tech> newList = new ArrayList<Tech>();
-                newList.add(currentList.remove(0));
-                while (currentList.size() > 0) {
-                    Tech t = currentList.remove(0);
-                    int currentMatch = -1;
-                    Tech tM = null;
-                    for (int i = 0; i < newList.size(); i++) {
-                        Tech tN = newList.get(i);
-                        if (tN == t)
-                            continue;
-                        CivLog.info(t.toString());
-                        Map<Tech, Integer> mp2 = relationship.get(t);
-                        CivLog.info(mp2.toString());
-                        int rel = mp2.get(tN);
-                        if (rel > currentMatch) {
-                            currentMatch = rel;
-                            tM = tN;
-                        }
-                    }
-                    int tMi = newList.indexOf(tM);
-                    
-                    int nextBest = tMi;
-                    Tech tNB = newList.get(tMi);
-                    if (tNB == tM)
-                        continue;
-                    if (relationship.get(t).get(tNB) > currentMatch)
-                        newList.add(tMi, t);
-                    else
-                        newList.add(tMi + 1, t);
-                }
-                techList.set(idx, newList);
+            int size = currentList.size();
+            currentList.clear();
+            List<Double> keys = Arrays.asList(newBests.keySet().toArray(new Double[0]));
+            Collections.sort(keys);
+            for (double key : keys) {
+                int pos = (int) key * size;
+                for (Tech t : newBests.get(key))
+                    currentList.add(t);
             }
-            
-            StringBuilder builder = new StringBuilder();
-            for (Entry<Tech, Map<Tech, Integer>> mts : relationship.entrySet()) {
-                builder.append("\n");
-                builder.append(mts.getKey().getLocalizedName());
-                builder.append(" {\n");
-                for (Entry<Tech, Integer> mtsInt : mts.getValue().entrySet()) {
-                    builder.append("\t");
-                    builder.append(mtsInt.getKey().getLocalizedName());
-                    builder.append(" = ");
-                    builder.append(mtsInt.getValue().toString());
-                    builder.append("\n");
-                }
-                builder.append("}");
-            }
-            
-            CivLog.info(builder.toString());
-            // TODO: stuffs here
         }
+        
+//        for (int idx = 1; idx < techList.size(); idx++) {
+//            lastList = techList.get(idx - 1);
+//            if (techList.size() > idx + 1)
+//                nextList = techList.get(idx + 1);
+//            else
+//                nextList = null;
+//            List<Tech> currentList = techList.get(idx);
+//            
+//            // Basically, add a relationship between each tech and each other tech in this list.
+//            Map<Tech, Map<Tech, Integer>> relationship = new HashMap<Tech, Map<Tech, Integer>>();
+//            for (int j = 0; j < currentList.size(); j++) {
+//                Tech t0 = currentList.get(j);
+//                Map<Tech, Integer> mp = new HashMap<Tech, Integer>();
+//                relationship.put(t0, mp);
+//                for (int k = 0; k < currentList.size(); k++) {
+//                    Tech t1 = currentList.get(k);
+//                    if (t1 == t0)
+//                        continue;
+//                    int shared = 0;
+//                    
+//                    if (nextList != null) {
+//                        List<Tech> t1Children = Arrays.asList(t1.getChildTechs());
+//                        for (int l = 0; l < nextList.size(); l++) {
+//                            Tech t = nextList.get(l);
+//                            for (Tech t00 : t0.getChildTechs())
+//                                if (t1Children.contains(t00))
+//                                    shared++;
+//                        }
+//                    }
+//                    
+//                    List<Tech> t1Parents = Arrays.asList(t1.getParentTechs());
+//                    for (int l = 0; l < lastList.size(); l++) {
+//                        Tech t = lastList.get(l);
+//                        for (Tech t00 : t0.getParentTechs())
+//                            if (t1Parents.contains(t00))
+//                                shared++;
+//                    }
+//                    
+//                    mp.put(t1, shared);
+//                }
+//            }
+//            
+//            for (int j = 0; j < currentList.size(); j++) {
+//                List<Tech> newList = new ArrayList<Tech>();
+//                newList.add(currentList.remove(0));
+//                while (currentList.size() > 0) {
+//                    Tech t = currentList.remove(0);
+//                    int currentMatch = -1;
+//                    Tech tM = null;
+//                    for (int i = 0; i < newList.size(); i++) {
+//                        Tech tN = newList.get(i);
+//                        if (tN == t)
+//                            continue;
+//                        CivLog.info(t.toString());
+//                        Map<Tech, Integer> mp2 = relationship.get(t);
+//                        CivLog.info(mp2.toString());
+//                        int rel = mp2.get(tN);
+//                        if (rel > currentMatch) {
+//                            currentMatch = rel;
+//                            tM = tN;
+//                        }
+//                    }
+//                    int tMi = newList.indexOf(tM);
+//                    
+//                    int nextBest = tMi;
+//                    Tech tNB = newList.get(tMi);
+//                    if (tNB == tM)
+//                        continue;
+//                    if (relationship.get(t).get(tNB) > currentMatch)
+//                        newList.add(tMi, t);
+//                    else
+//                        newList.add(tMi + 1, t);
+//                }
+//                techList.set(idx, newList);
+//            }
+//            
+//            StringBuilder builder = new StringBuilder();
+//            for (Entry<Tech, Map<Tech, Integer>> mts : relationship.entrySet()) {
+//                builder.append("\n");
+//                builder.append(mts.getKey().getLocalizedName());
+//                builder.append(" {\n");
+//                for (Entry<Tech, Integer> mtsInt : mts.getValue().entrySet()) {
+//                    builder.append("\t");
+//                    builder.append(mtsInt.getKey().getLocalizedName());
+//                    builder.append(" = ");
+//                    builder.append(mtsInt.getValue().toString());
+//                    builder.append("\n");
+//                }
+//                builder.append("}");
+//            }
+//            
+//            CivLog.info(builder.toString());
+//            // TODO: stuffs here
+//        }
     }
     
     private void populateDrawPositions(List<List<Tech>> techList, Map<Tech, Integer> tempMap) {
