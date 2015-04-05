e4d0788e6afe65fd1462d758ae3cc535a57ed8bd
AlexIIL <alexj9@me.com>
2015-04-05 18:46:22 +0100

    Actually fix the exploration bug. It turns out that "<< 4" multiplies by 16, not divide as is needed to convert BlockPos -> ChunkCoordIntPair.

--- src/main/java/alexiil/mods/civ/utils/CraftUtils.java
+++ src/main/java/alexiil/mods/civ/utils/CraftUtils.java
@@ -61,7 +61,7 @@ public class CraftUtils {
     }
 
     public static boolean addPlayerToChunk(World world, BlockPos pos, PlayerTechData techs) {
-        ChunkCoordIntPair ccip = new ChunkCoordIntPair(pos.getX() << 4, pos.getZ() << 4);
+        ChunkCoordIntPair ccip = new ChunkCoordIntPair(pos.getX() >> 4, pos.getZ() >> 4);
         List<PlayerTechData> list = getPlayers(world, ccip);
         boolean found = false;
         for (PlayerTechData ptd : list) {
@@ -74,11 +74,11 @@ public class CraftUtils {
     }
 
     public static List<PlayerTechData> getPlayers(World world, BlockPos pos) {
-        return getPlayers(world, new ChunkCoordIntPair(pos.getX() << 4, pos.getZ() << 4));
+        return getPlayers(world, new ChunkCoordIntPair(pos.getX() >> 4, pos.getZ() >> 4));
     }
 
     public static List<Tech> getTechs(World world, BlockPos pos) {
-        return getTechs(world, new ChunkCoordIntPair(pos.getX() << 4, pos.getZ() << 4));
+        return getTechs(world, new ChunkCoordIntPair(pos.getX() >> 4, pos.getZ() >> 4));
     }
 
     public static List<Tech> getTechs(World world, ChunkCoordIntPair ccip) {
