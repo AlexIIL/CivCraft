d4dee8c5e03b13f38cd72e899b77c22078708485
AlexIIL <alexj9@me.com>
2015-04-07 11:31:57 +0100

    Move help text to AlexIILLib

--- src/main/java/alexiil/mods/civ/gui/Config.java
+++ src/main/java/alexiil/mods/civ/gui/Config.java
@@ -1,120 +1,34 @@
 package alexiil.mods.civ.gui;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import alexiil.mods.civ.CivCraft;
-import alexiil.mods.civ.gui.ConfigGuiFactory.ActualConfig;
 import alexiil.mods.lib.LangUtils;
-import alexiil.mods.lib.git.BaseConfig;
+import alexiil.mods.lib.gui.BaseConfig;
 
 public class Config extends BaseConfig {
-    private GuiButton helpClose;
-    private boolean help = false;
-    private int xPosHelp = 0;
-    private List<List<String>> helpText;
-
     public Config(GuiScreen screen) {
         super(screen, CivCraft.instance);
-    }
-
-    protected void setupGui() {
-        super.setupGui();
-        int index = 0;
-        int maxXPos = Math.min(this.width - xPosHelp, 400);
-
-        helpText = new ArrayList<List<String>>();
-        while (true) {
-            String preTranslation = "civcraft.gui.config.help." + index;
-            String text = LangUtils.format(preTranslation);
-            if (preTranslation.equals(text))
-                break;
-            String[] strings1 = text.split("\n");
-            for (int i = 0; i < strings1.length; i++) {
-                String s = strings1[i];
-                String nextLine = "";
-                while (fontRendererObj.getStringWidth(s) > maxXPos && s != null) {
-                    if (s.length() <= 10)
-                        break;
-                    nextLine = s.substring(s.length() - 1) + nextLine;
-                    s = s.substring(0, s.length() - 1);
-                }
-                if (nextLine.length() > 0) {
-                    strings1 = Arrays.copyOf(strings1, strings1.length + 1);
-                    strings1[i] = s;
-                    strings1[i + 1] = nextLine;
-                }
-            }
-            String[] strings = strings1;
-            helpText.add(Arrays.asList(strings));
-            index++;
-        }
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public void initGui() {
-        String text = LangUtils.format("civcraft.config.button");
+        super.initGui();
+        String text = LangUtils.format("civcraft.config.tech.button");
         int length = fontRendererObj.getStringWidth(text) + 20;
-        int totalLength = 10;
-        buttonList.add(new GuiButton(0, totalLength, 1, length, 20, text));
+        buttonList.add(new GuiButton(3, totalLength, 1, length, 20, text));
         totalLength += length;
-
-        text = LangUtils.format("civcraft.config.tech.button");
-        length = fontRendererObj.getStringWidth(text) + 20;
-        buttonList.add(new GuiButton(1, totalLength, 1, length, 20, text));
-        totalLength += length;
-
-        text = LangUtils.format("civcraft.config.help");
-        length = fontRendererObj.getStringWidth(text) + 20;
-        buttonList.add(new GuiButton(2, totalLength, 1, length, 20, text));
-        xPosHelp = totalLength;
-        totalLength += length;
-
-        text = LangUtils.format("civcraft.config.closeHelp");
-        length = fontRendererObj.getStringWidth(text) + 20;
-        helpClose = new GuiButton(3, totalLength, 1, length, 20, text);
-        helpClose.visible = false;
-        buttonList.add(helpClose);
-    }
-
-    @Override
-    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-        super.drawScreen(mouseX, mouseY, partialTicks);
-
-        if (help) {
-            int yHeight = 0;
-            for (List<String> ss : helpText)
-                yHeight += fontRendererObj.FONT_HEIGHT * (ss.size() + 1);
-            drawGradientRect(xPosHelp, 40, xPosHelp + 440, yHeight + 80, 0xFF000000, 0xFF000000);
-            int yPos = 60;
-            for (List<String> strings : helpText) {
-                drawHoveringText(strings, xPosHelp, yPos);
-                yPos += (strings.size() + 2) * fontRendererObj.FONT_HEIGHT;
-            }
-        }
     }
 
     @Override
     protected void actionPerformed(GuiButton button) throws IOException {
-        if (button.id == 0) {
-            Minecraft.getMinecraft().displayGuiScreen(new ActualConfig(this));
-        }
-        if (button.id == 1) {
+        super.actionPerformed(button);
+        if (button.id == 3) {
             Minecraft.getMinecraft().displayGuiScreen(new TechConfig(this));
-        }
-        if (button.id == 2) {
-            help = true;
-            helpClose.visible = true;
-        }
-        if (button.id == 3 && helpClose.visible) {
-            help = false;
-            helpClose.visible = false;
         }
     }
 }
--- src/main/java/alexiil/mods/civ/gui/ConfigGuiFactory.java
+++ src/main/java/alexiil/mods/civ/gui/ConfigGuiFactory.java
@@ -1,36 +1,12 @@
 package alexiil.mods.civ.gui;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraftforge.common.config.ConfigElement;
-import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.client.IModGuiFactory;
-import net.minecraftforge.fml.client.config.GuiConfig;
-import net.minecraftforge.fml.client.config.IConfigElement;
-import alexiil.mods.civ.CivCraft;
-import alexiil.mods.civ.Lib;
-import alexiil.mods.lib.LangUtils;
 
 public class ConfigGuiFactory implements IModGuiFactory {
-    public static class ActualConfig extends GuiConfig {
-        public ActualConfig(GuiScreen parent) {
-            super(parent, getConfigElements(), Lib.Mod.ID, false, false, LangUtils.format("civcraft.config.title"));
-        }
-
-        private static List<IConfigElement> getConfigElements() {
-            List<IConfigElement> elements = new ArrayList<IConfigElement>();
-            Configuration cfg = CivCraft.instance.cfg.cfg;
-            elements.add(new ConfigElement(cfg.getCategory(Configuration.CATEGORY_GENERAL)));
-            elements.add(new ConfigElement(cfg.getCategory("blocks")));
-            elements.add(new ConfigElement(cfg.getCategory("items")));
-            return elements;
-        }
-    }
-
     @Override
     public void initialize(Minecraft minecraftInstance) {}
 
